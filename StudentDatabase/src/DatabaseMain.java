be9e87b5a133dc2a3f5844d645079f1dead0c90b
tim <tfierek357@gmail.com>
2022-04-12 12:11:06 -0400

    first commit, empty classes with javadoc comments

---
+++ StudentDatabase/src/DatabaseMain.java
@@ -0,0 +1,21 @@
+
+/**
+ * DatabaseMain handles all exception handling and user input for the 
+ * student database.
+ * 
+ * @author Tim Fierek
+ *
+ */
+public class DatabaseMain {
+
+	/**
+	 * Collects console input and catches and handles exceptions and 
+	 * incorrect input, also prints data out.
+	 * 
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
