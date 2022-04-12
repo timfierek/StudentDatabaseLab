282efbeb913df263bbafa52efb7b052d8e2bd524
tim <tfierek357@gmail.com>
2022-04-12 13:52:54 -0400

    Added constructor for Student.java. added getter and setter methods for Student.java data. Initialized student array in DatabaseMain.java

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/cb/d0c7037087ba001c1060b4555af704d9
@@ -0,0 +1,14 @@
+
+/**
+ * Student.java contains the constructor and various methods for a 
+ * student object to be used in the student database and accessed in
+ * DatabaseMain.java
+ * 
+ * @author Tim Fierek
+ *
+ */
+public class Student {
+	
+	
+	
+}
--- StudentDatabase/src/DatabaseMain.java
+++ StudentDatabase/src/DatabaseMain.java
@@ -14,8 +14,17 @@ public class DatabaseMain {
 	 * 
 	 */
 	public static void main(String[] args) {
-		
-
+		Student[] studentDatabase = new Student[10];
+		studentDatabase[0] = new Student("Tim Fierek", "Steak", "Dexter");
+		studentDatabase[1] = new Student("Ali Beatriz", "Popcorn", "Ann Arbor");
+		studentDatabase[2] = new Student("Charles Diya", "Apples", "Flint");
+		studentDatabase[3] = new Student("Eric Fatima", "Coffee", "East Lansing");
+		studentDatabase[4] = new Student("Gabriel Hanna", "Hotdogs", "Houghton");
+		studentDatabase[5] = new Student("Kendrick Lammar", "Cheese", "Mackinac");
+		studentDatabase[6] = new Student("Yung Gravy", "Caviar", "Novi");
+		studentDatabase[7] = new Student("Jennifer Lopez", "Goldfish", "Ypsilanti");
+		studentDatabase[8] = new Student("Elon Musk", "Cat food", "Holland");
+		studentDatabase[9] = new Student("John Doe", "Lettuce", "Muskegon");
 	}
 
 }
